7aa7e0bd9a7cd607526bbe5ee6b9450ba8f78868
Mariley20 <mariley2018@gmail.com>
2017-10-06 15:00:52 -0500

    commit

--- app.jsx
+++ app.jsx
@@ -199,4 +199,72 @@ class App extends React.Component {
 		);
 	}
 }
+const css = [
+	{
+		srcImg: "img/css/1.png",
+		title: "How to Make a Website",
+		content: "If you’ve never built a website before and you have no coding or design experience, this is the place to start. In this project, we learn how to build a modern portfolio website for desktops, tablets, and mobile devices. We start with basic HTML and CSS syntax. Next, we learn how to build custom web pages with an image gallery and contact page. Finally, we walk through how to share a website live on the web."
+	},
+	{
+		srcImg: "img/css/2.png",
+		title: "HTML Forms",
+		content: "The web is a two-way communication medium. There’s lots of HTML elements for displaying data and producing output, and conversely, there’s also lots of HTML elements for accepting input. Accepting input from the user means creating web forms. In this course, we’ll learn about all the most important form elements that web professionals use on a daily basis."
+	},
+	{
+		src: "img/css/3.png",
+		title: "HTML Video and Audio",
+		content: "Text and images have always been the foundation of web content, but more than ever, video and audio are also a part of that content mix. Fortunately, we can now create standards-based video and audio players that don't require the use of plugins. Adding video and audio to a webpage is almost as easy as adding an image or formatting some text."
+	},
+	{
+		src: "img/css/4.png",
+		title: "SVG Basics",
+		content: "Scalable Vector Graphics (SVG) is an XML markup language for creating two-dimensional images using vectors. This is different from traditional raster-based image formats that use pixels, like JPEG and PNG. When used on web pages, SVG images provide an infinite level of detail, so they look sharp regardless of screen size or pixel density."
+	},
+	{
+		src: "img/css/5.png",
+		title: "Responsive Images",
+		content: "Using the new source-set and sizes attributes, and the new picture element, it's possible to create images that behave better in a responsive design. These new pieces of markup allow us to deliver the right image to the right device, based on resolution, pixel density, and other factors we define. This will help web pages load faster and look better, and the most capable devices will get the best looking images possible."
+	}, {
+		src: "img/css/6.png",
+		title: "Introduction to HTML and CSS",
+		content: "Get started creating web pages with HTML and CSS, the basic building blocks of web development. HTML, or Hypertext Markup Language, is a standard set of tags you will use to tell the web browser how the content of your web pages and applications are structured. Use CSS, or Cascading Style Sheets, to select HTML tags and tell the browser what your content should look like."
+	}
+];
+const html = [
+	{
+		srcImg: "img/html/1.png",
+		title: "How to Make a Website",
+		content: "If you’ve never built a website before and you have no coding or design experience, this is the place to start. In this project, we learn how to build a modern portfolio website for desktops, tablets, and mobile devices. We start with basic HTML and CSS syntax. Next, we learn how to build custom web pages with an image gallery and contact page. Finally, we walk through how to share a website live on the web."
+
+	},
+	{
+		srcImg: "img/html/2.png",
+		title: "HTML Forms",
+
+		content: "The web is a two-way communication medium. There’s lots of HTML elements for displaying data and producing output, and conversely, there’s also lots of HTML elements for accepting input. Accepting input from the user means creating web forms. In this course, we’ll learn about all the most important form elements that web professionals use on a daily basis."
+	},
+	{
+		srcImg: "img/html/3.png",
+		title: "HTML Video and Audio",
+
+		content: "Text and images have always been the foundation of web content, but more than ever, video and audio are also a part of that content mix. Fortunately, we can now create standards-based video and audio players that don't require the use of plugins. Adding video and audio to a webpage is almost as easy as adding an image or formatting some text."
+	},
+	{
+		srcImg: "img/html/4.png",
+		title: "SVG Basics",
+
+		content: "Scalable Vector Graphics (SVG) is an XML markup language for creating two-dimensional images using vectors. This is different from traditional raster-based image formats that use pixels, like JPEG and PNG. When used on web pages, SVG images provide an infinite level of detail, so they look sharp regardless of screen size or pixel density."
+	},
+	{
+		srcImg: "img/html/5.png",
+		title: "Responsive Images",
+
+		content: " Using the new source-set and sizes attributes, and the new picture element, it's possible to create images that behave better in a responsive design. These new pieces of markup allow us to deliver the right image to the right device, based on resolution, pixel density, and other factors we define. This will help web pages load faster and look better, and the most capable devices will get the best looking images possible."
+	},
+	{
+		srcImg: "img/html/6.png",
+		title: "Introduction to HTML and CSS",
+		content: "Get started creating web pages with HTML and CSS, the basic building blocks of web development. HTML, or Hypertext Markup Language, is a standard set of tags you will use to tell the web browser how the content of your web pages and applications are structured. Use CSS, or Cascading Style Sheets, to select HTML tags and tell the browser what your content should look like."
+	}
+];
 ReactDOM.render(<App />, document.getElementById("root"));
